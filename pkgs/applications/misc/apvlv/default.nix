612d9f69e1edf1f1b4b1d938fb26b7bbd896b248
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-05 10:48:37 +0200

    Compile apvlv but freeze on runtime

---
+++ pkgs/applications/misc/apvlv/default.nix
@@ -0,0 +1,66 @@
+{ stdenv
+, fetchurl
+, cmake
+, pkgconfig
+, gtk3
+, poppler
+, poppler_data
+# , poppler_utils
+, freetype
+, libpthreadstubs
+, libXdmcp
+, libxshmfence
+, libxkbcommon
+, epoxy
+# , git
+# , tree
+}:
+
+stdenv.mkDerivation rec {
+  # version = "0.1.f7f7b9c";
+  version = "0.1.5";
+  name = "apvlv-${version}";
+
+  src = fetchurl {
+    # url = "https://github.com/downloads/naihe2010/apvlv/${name}-Source.tar.gz";
+    # sha256 = "1rbp54mr3z2x3a3a1qmz8byzygzi223vckfam9ib5g1sfds0qf8i";
+    url = "https://github.com/naihe2010/apvlv/archive/v${version}.tar.gz";
+    sha256 = "12ha212a8c9z02q3in47ggf1762l4vm52isisq9fr7rwwrbq3afw";
+  };
+
+  preConfigure = ''
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${poppler}/include/poppler"
+  '';
+
+  buildInputs = [
+    pkgconfig cmake
+     poppler poppler_data
+     freetype gtk3 libpthreadstubs libXdmcp libxshmfence libxkbcommon epoxy
+   ];
+
+  installPhase = ''
+    # echo "pwd: `pwd`"
+    # tree ..
+    # echo "out: $out"
+    # tree $out
+
+    # binary
+    mkdir -p $out/bin
+    cp src/apvlv $out/bin/apvlv
+    chmod +x $out/bin/apvlv
+
+    # pdf startup as doc
+    mkdir -p $out/share/doc/apvlv/
+    cp ../Startup.pdf $out/share/doc/apvlv/Startup.pdf
+    cp ../main_menubar.glade $out/share/doc/apvlv/
+  '';
+
+  meta = {
+    homepage = "http://naihe2010.github.io/apvlv/";
+    description = "PDF viewer with Vim-like behaviour";
+
+    platforms = stdenv.lib.platforms.unix;
+    maintainers = [ stdenv.lib.maintainers.ardumont ];
+  };
+
+}
