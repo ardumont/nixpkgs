064ae6cb4fcd596f86faec6535a4d9c34d2d3a49
Jordi Masip <jordi@masip.cat>
2020-05-24 16:20:22 +0200

    Revert "libcroco: init at 0.6.13"
    This reverts commit c75b6d16b32f6b840bbe1fbd7b653a79276aaadf.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4903,8 +4903,6 @@ in
 
   libcoap = callPackage ../applications/networking/libcoap {};
 
-  libcroco = callPackage ../development/libraries/libcroco { };
-
   libcryptui = callPackage ../development/libraries/libcryptui { };
 
   libsmi = callPackage ../development/libraries/libsmi { };
